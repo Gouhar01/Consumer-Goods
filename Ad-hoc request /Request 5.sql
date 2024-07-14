5f7108d17af3f17aa5f5239654184d88a72bd927
Gouhar01 <141431067+Gouhar01@users.noreply.github.com>
2024-07-14 20:24:07 +0530

    Create Request 5.sql

---
+++ Ad-hoc request /Request 5.sql
@@ -0,0 +1,13 @@
+# 5.  Get the products that have the highest and lowest manufacturing costs. The final output should contain these fields, 
+product code , product , manufacturing cost.
+
+
+SELECT p.product_code,p.product,ROUND(m.manufacturing_cost,2) AS  manufacturing_cost
+FROM dim_product p INNER JOIN fact_manufacturing_cost m 
+USING(product_code)
+WHERE m.manufacturing_cost = (SELECT MAX(manufacturing_cost) FROM fact_manufacturing_cost)
+UNION ALL
+SELECT p.product_code,p.product,ROUND(m.manufacturing_cost,2) AS  manufacturing_cost
+FROM dim_product p INNER JOIN fact_manufacturing_cost m 
+USING(product_code)
+WHERE m.manufacturing_cost = (SELECT MIN(manufacturing_cost) FROM fact_manufacturing_cost)
